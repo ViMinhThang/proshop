7c2e31c34f86ca0c8c455fc17061fe41b28dcadf
ViMinhThang <huynhminhthang@gmail.com>
2024-09-16 15:08:56 +0700

    jwt stuff (finally works i swear)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,39 @@
 import asyncHanlder from "../middleware/asyncHandler.js";
 import User from "../models/productModel.js";
+import jwt from "jsonwebtoken";
 
 // @desc        Auth user & get token
 // @route       POST /api/users/login
 // @access      Public
-
 const authUser = asyncHanlder(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+
+    // Set JWT as HTTP-Only cookie
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 30 * 24 * 60 * 60 * 1000 // 30 Days,
+    });
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+
   res.send("auth user");
 });
 
@@ -51,7 +79,7 @@ const getUsers = asyncHanlder(async (req, res) => {
 // @desc        Get user by ID
 // @route       GET /api/users/:id
 // @access      Private/Admin
-const getUserByID = asyncHanlder(async (req, res) => {
+const getUserById = asyncHanlder(async (req, res) => {
   res.send("get users");
 });
 
@@ -77,6 +105,6 @@ export {
   updateUserProfile,
   getUsers,
   deleteUser,
-  getUserByID,
+  getUserById,
   updateUser,
 };
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
 import productRouter from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
-
 connectDB();
 
 const app = express();
+
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Cookie parser middleware
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
 app.use("/api/products", productRouter);
+app.use("/api/users", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 app.listen(port, () => console.log(`Server is running on port ${port}`));
