f7e3af5c53d1d3f5e07a6ed7584454df1b019470
ViMinhThang <huynhminhthang246@gmail.com>
2025-05-26 16:29:45 +0700

    prod is breakingggg

--- backend/server.js
+++ backend/server.js
@@ -42,4 +42,4 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(notFound);
 app.use(errorHandler);
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port,'0.0.0.0', () => console.log(`Server is running on port ${port}`));
